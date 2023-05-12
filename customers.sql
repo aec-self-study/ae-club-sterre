9beb0a767176cb87fde82cc0d1a660552aa53c38
SterrevanderPoll <122735014+SterrevanderPoll@users.noreply.github.com>
2023-05-12 09:00:02 +0000

    Add customers query 2

---
+++ customers.sql
@@ -0,0 +1,12 @@
+SELECT customer_id
+,name
+,email
+
+,min(created_at) as first_order_at
+,count(*) as number_of_orders
+FROM `analytics-engineers-club.coffee_shop.orders`
+LEFT JOIN `analytics-engineers-club.coffee_shop.customers`
+ON `analytics-engineers-club.coffee_shop.orders`.customer_id = `analytics-engineers-club.coffee_shop.customers`.id
+GROUP BY customer_id, name, email
+ORDER BY first_order_at
+LIMIT 5
