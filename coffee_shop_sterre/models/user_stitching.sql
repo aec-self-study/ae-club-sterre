0524d3c2217e111b04eaebe624b9e38053242f9b
SterrevanderPoll <122735014+SterrevanderPoll@users.noreply.github.com>
2023-06-21 19:50:30 +0000

    message

--- coffee_shop_sterre/models/user_stitching.sql
+++ coffee_shop_sterre/models/user_stitching.sql
@@ -2,8 +2,10 @@
     materialized='table'
 )}}
 
-select customer_id, count(distinct visitor_id)
-  from {{ source('web_tracking', 'pageviews') }} as pageviews
+select 
+  customer_id, 
+  count(distinct visitor_id) as count
+from {{ source('web_tracking', 'pageviews') }} as pageviews
 where customer_id is not null
 group by 1 
 order by 2 desc 
